a2686592bf3ba9392a110d1f725d07ca23376b45
mikepan <mikepan@f6d0cfac-5bcb-11de-b31f-81d54249e7d9>
2010-02-01 06:14:45 +0000

    fixed compile issues

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     name='ftgl',
     version='0.1',
     ext_modules=[
-        Extension('FTGL', ['ftgl.sip']),
+        Extension('FTGL', 
+                  ['ftgl.sip'],
+                  library_dirs=['/usr/local/lib'],
+                  libraries=['ftgl']),
     ],
     include_dirs=['.', '/usr/local/include', '/usr/local/include/freetype2'],
     cmdclass = {'build_ext':sipdistutils.build_ext}
