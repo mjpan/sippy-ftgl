bc65790f458b1b90fba074101cf0cf7082761b2f
mikepan <mikepan@f6d0cfac-5bcb-11de-b31f-81d54249e7d9>
2010-02-07 20:11:56 +0000

    add metadata for setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension, find_packages
+
 import sipdistutils
 
 setup(
-    name='ftgl',
-    version='0.1',
+    name='sippy-ftgl',
+    version='0.1.0',
     ext_modules=[
         Extension('FTGL', 
                   ['ftgl.sip'],
@@ -11,6 +12,17 @@ setup(
                   libraries=['ftgl']),
     ],
     include_dirs=['.', '/usr/local/include', '/usr/local/include/freetype2'],
-    cmdclass = {'build_ext':sipdistutils.build_ext}
+    cmdclass = {'build_ext':sipdistutils.build_ext},
+
+    # metadata for upload to PyPI
+    author = "michael j pan",
+    author_email = "mikepan@gmail.com",
+    description = "Python binding for FTGL using SIP",
+    license = "New BSD",
+    keywords = "ftgl SIP",
+    url = "http://code.google.com/p/sippy-ftgl/",
+    long_description = "This is a Python binding for FTGL using SIP",
+    platforms = ["All"]
+
 )
 
