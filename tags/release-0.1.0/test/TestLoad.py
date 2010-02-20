ff6704fedac8bb2f212d021870a2cb29febcdbb8
mikepan <mikepan@f6d0cfac-5bcb-11de-b31f-81d54249e7d9>
2010-02-20 22:59:56 +0000

    tagging for 0.1.0 release

---
+++ tags/release-0.1.0/test/TestLoad.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestLoad(unittest.TestCase):
+
+    def testImport(self):
+        import FTGL
+        return
+
+    def testCreateBbox(self):
+        import FTGL
+        b = FTGL.BBox()
+        return
+
+    pass
